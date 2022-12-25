c554e278d3c28f2f48a2271622edf1aca8d4107d
kaandonmez <kaandonmeztf@gmail.com>
2022-12-25 23:00:44 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 97);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(119, 341);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(137, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(119, 341);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 107);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "SET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 13);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(35, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Lines:";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(134, 13);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(35, 13);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Lines:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(262, 134);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(62, 125);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(330, 384);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+
+        //Create regex to detect if the string is empty
+        Regex regex = new Regex(@"^\s*$");
+
+        //Create regx to detect \r\n
+        Regex regex2 = new Regex(@"\r\n");
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //clean richTextBox1
+            richTextBox1.Text = "";
+
+            //read dictionary from test1.txt
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            string[] lines = System.IO.File.ReadAllLines("test1.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('=');
+                dict.Add(parts[0], parts[1]);
+            }
+
+            //check every line in textbox1 for a match in the dictionary if found write to richTextBox1 , if not write not found to richTextBox1
+            string[] lines2 = textBox1.Lines;
+            foreach (string line in lines2)
+            {
+
+
+
+                //if textbox1 line is empty delete that line in textbox1
+                if (regex.IsMatch(line) || line == "" || line == " " || regex2.IsMatch(line))
+                {
+
+                    var array = textBox1.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                    textBox1.Text = string.Join(Environment.NewLine, array);
+
+
+                }
+
+
+
+                else
+                {
+                    if (dict.ContainsKey(line))
+                    {
+                        richTextBox1.Text += dict[line] + Environment.NewLine;
+                    }
+                    else
+                    {
+                        richTextBox1.Text += "Not found" + Environment.NewLine;
+                    }
+                }
+
+
+
+            }
+
+            //paint every "Not found" in red in richTextBox1
+            //int index = 0;
+            //while (index < richTextBox1.Text.LastIndexOf("Not found"))
+            //{
+            //    richTextBox1.Find("Not found", index, richTextBox1.TextLength, RichTextBoxFinds.None);
+            //    richTextBox1.SelectionColor = Color.Red;
+            //    index = richTextBox1.Text.IndexOf("Not found", index) + 1;
+            //}
+           
+            //delete last \n in richTextbox1
+            richTextBox1.Text = richTextBox1.Text.TrimEnd('\n');
+            
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //set linklabel1 to textbox1 line count
+            linkLabel1.Text = "Lines: "+textBox1.Lines.Length.ToString();
+        }
+
+        //scroll same time in both richtextbox and textbox
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = richTextBox1.SelectionStart;
+            textBox1.SelectionLength = richTextBox1.SelectionLength;
+            textBox1.ScrollToCaret();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //add to the dictionary from textbox1 and richTextbox1, check for conflicts
+            //key is textbox1 and value is richTextBox1
+            //the dictionary is in test1.txt split by =
+            //if there is a conflict ask the user if he wants to overwrite the value
+            //if the user says yes overwrite the value
+            //if the user says no do nothing
+
+            //read dictionary from test1.txt
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            string[] lines = System.IO.File.ReadAllLines("test1.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('=');
+                dict.Add(parts[0], parts[1]);
+            }
+
+            
+
+            
+            //check if textbox1 and richTextBox1 have the same number of lines
+           if (textBox1.Lines.Length != richTextBox1.Lines.Length)
+            {
+                MessageBox.Show("Textbox1 and RichTextbox1 have different number of lines");
+            }
+
+            //check if textbox1 and richTextBox1 have the same number of lines
+            else if (textBox1.Lines.Length == richTextBox1.Lines.Length)
+            {
+                //check if there is a conflict
+                for (int i = 0; i < textBox1.Lines.Length; i++)
+                {
+                    if (dict.ContainsKey(textBox1.Lines[i]))
+                    {
+                        //ask the user if he wants to overwrite the value, show the value that will be overwritten, show the new value and show the key
+                        DialogResult dialogResult = MessageBox.Show("Do you want to overwrite the value? " + Environment.NewLine + "Key: " + textBox1.Lines[i] + Environment.NewLine + "Old value: " + dict[textBox1.Lines[i]] + Environment.NewLine + "New value: " + richTextBox1.Lines[i], "Overwrite value", MessageBoxButtons.YesNo);
+                        
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            //overwrite the value
+                            dict[textBox1.Lines[i]] = richTextBox1.Lines[i];
+                        }
+                        else if (dialogResult == DialogResult.No)
+                        {
+                            //do nothing
+                        }
+                    }
+                    else
+                    {
+                        //add to the dictionary
+                        dict.Add(textBox1.Lines[i], richTextBox1.Lines[i]);
+                    }
+                }
+
+                //write the dictionary to test1.txt
+                System.IO.File.WriteAllText("test1.txt", string.Empty);
+                foreach (KeyValuePair<string, string> entry in dict)
+                {
+                    System.IO.File.AppendAllText("test1.txt", entry.Key + "=" + entry.Value + Environment.NewLine);
+                }
+            }
+
+
+            //set textbox2 to total words in dictionary
+            textBox2.Text = "Total: "+dict.Count.ToString();
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            //set linklabel2 richTextbox1 line count
+            linkLabel2.Text = "Lines: " + richTextBox1.Lines.Length.ToString();
+            
+
+        }
+    }
+}
